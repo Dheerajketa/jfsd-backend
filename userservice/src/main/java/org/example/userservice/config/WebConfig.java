70b1640359099bf2415b4a6fd52c51a0a7fc4aab
Dheerajketa <2200032161@kluniversity.in>
2024-12-07 23:20:16 +0530

    testing the

--- userservice/src/main/java/org/example/userservice/config/WebConfig.java
+++ userservice/src/main/java/org/example/userservice/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","https://jfsd-react-frontend.vercel.app/")
+                .allowedOrigins("http://localhost:3000","https://jfsd-react-frontend.vercel.app/","userservice.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
